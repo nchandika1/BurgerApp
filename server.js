1a607d337c34ff7e55de6faf87a0a166fe1aff30
nchandika1 <nchandika@gmail.com>
2017-11-19 08:21:24 -0800

    heroku deployable

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
 
-const PORT = 3000;  // Port to listen to
+const PORT = process.env.PORT || 3000;  // Port to listen to
 
 var app = express();  // Get express handle
 
