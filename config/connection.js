c41fa95b924be467bd22137d302a1108f4fb5420
nchandika1 <nchandika@gmail.com>
2017-11-19 12:35:25 -0800

    Heroku connection added

--- config/connection.js
+++ config/connection.js
@@ -4,15 +4,20 @@
 
 // Dependencies
 var mysql = require("mysql");
+var connection;
 
-// Create connection parameters
-var connection = mysql.createConnection({
-	port: 3306,
-	host: "127.0.0.1",
-	user: "root",
-	password: "",
-	database: "burgers_db"
-});
+// Create connection parameters so that it is also deployed on HEROKU
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		port: 3306,
+		host: "127.0.0.1",
+		user: "root",
+		password: "",
+		database: "burgers_db"
+	});
+};
 
 // Connection to the burgers_db
 connection.connect(function(err) {
